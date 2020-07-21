f7a8562021743a77e40723ed832b565214767601
Valeria Avila <valeria.avila17@gmail.com>
2020-07-21 03:12:28 -0700

    Added the LittleLoop method

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -7,12 +7,25 @@ namespace HelloSourceControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            WriteStuff();
+            LittleLoop();
         }
         public static void WriteStuff()
         {
             Console.WriteLine("I am writing stuff...");
         }
 
-       
+        public static void LittleLoop()
+        {
+            for (int i = 1; i <= 5; i++) 
+            { 
+                for (int j = 1; j <=i; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
